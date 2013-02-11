17b9e75aded05d586449a7c811f1a54ee39c45b4
Ross Wintle <ross@oikos.org.uk>
2013-02-11 12:24:27 +0000

    Initial commit of v0.1 beta

---
+++ class-oikos-twitter-widget.php
@@ -0,0 +1,277 @@
+<?php
+if (! class_exists('tmhOAuth')) {
+	require 'tmhOAuth-master/tmhOAuth.php';
+	require 'tmhOAuth-master/tmhUtilities.php';
+}
+
+class Oikos_TwitterWidget extends WP_Widget {
+    /** constructor */
+    function Oikos_TwitterWidget() {
+        $options = array( 'description' => "Displays the Twitter widget" );
+        parent::WP_Widget(false, $name = 'Oikos Twitter Widget', $options);	
+    }
+
+    // This calculates a relative time, e.g. "1 minute ago"
+    private static function relativeTime($time)
+    {   
+        $second = 1;
+        $minute = 60 * $second;
+        $hour = 60 * $minute;
+        $day = 24 * $hour;
+        $month = 30 * $day;
+        
+        $delta = time() - $time;
+
+        if ($delta < 1 * $minute)
+        {
+            return $delta == 1 ? "one second ago" : $delta . " seconds ago";
+        }
+        if ($delta < 2 * $minute)
+        {
+          return "a minute ago";
+        }
+        if ($delta < 45 * $minute)
+        {
+            return floor($delta / $minute) . " minutes ago";
+        }
+        if ($delta < 90 * $minute)
+        {
+          return "an hour ago";
+        }
+        if ($delta < 24 * $hour)
+        {
+          return floor($delta / $hour) . " hours ago";
+        }
+        if ($delta < 48 * $hour)
+        {
+          return "yesterday";
+        }
+        if ($delta < 30 * $day)
+        {
+            return floor($delta / $day) . " days ago";
+        }
+        if ($delta < 12 * $month)
+        {
+          $months = floor($delta / $day / 30);
+          return $months <= 1 ? "one month ago" : $months . " months ago";
+        }
+        else
+        {
+            $years = floor($delta / $day / 365);
+            return $years <= 1 ? "one year ago" : $years . " years ago";
+        }
+    }    
+
+	// With thanks to The Danger Bees for the donkey work: http://dmblog.com/2011/08/how-to-use-tweet-entities/
+	private static function linkify_tweet($raw_text, $tweet = NULL)
+	{
+		// first set output to the value we received when calling this function
+		$output = $raw_text;
+
+		// create xhtml safe text (mostly to be safe of ampersands)
+		$output = htmlentities(html_entity_decode($raw_text, ENT_NOQUOTES, 'UTF-8'), ENT_NOQUOTES, 'UTF-8');
+
+		// parse urls
+		if ($tweet == NULL)
+		{
+			// for regular strings, just create <a> tags for each url
+			$pattern        = '/([A-Za-z]+:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&\?\/.=]+)/i';
+			$replacement    = '<a href="${1}" rel="external">${1}</a>';
+			$output         = preg_replace($pattern, $replacement, $output);
+		} else {
+			// for tweets, let's extract the urls from the entities object
+			foreach ($tweet->entities->urls as $url)
+			{
+				$old_url        = $url->url;
+				$expanded_url   = (empty($url->expanded_url))   ? $url->url : $url->expanded_url;
+				$display_url    = (empty($url->display_url))    ? $url->url : $url->display_url;
+				$replacement    = '<a href="'.$expanded_url.'" rel="external">'.$old_url.'</a>';
+				$output         = str_replace($old_url, $replacement, $output);
+			}
+
+			// let's extract the hashtags from the entities object
+			foreach ($tweet->entities->hashtags as $hashtags)
+			{
+				$hashtag        = '#'.$hashtags->text;
+				$replacement    = '<a href="http://twitter.com/search?q=%23'.$hashtags->text.'" rel="external">'.$hashtag.'</a>';
+				$output         = str_ireplace($hashtag, $replacement, $output);
+			}
+
+			// let's extract the usernames from the entities object
+			foreach ($tweet->entities->user_mentions as $user_mentions)
+			{
+				$username       = '@'.$user_mentions->screen_name;
+				$replacement    = '<a href="http://twitter.com/'.$user_mentions->screen_name.'" rel="external" title="'.$user_mentions->name.' on Twitter">'.$username.'</a>';
+				$output         = str_ireplace($username, $replacement, $output);
+			}
+
+			// if we have media attached, let's extract those from the entities as well
+			if (isset($tweet->entities->media))
+			{
+				foreach ($tweet->entities->media as $media)
+				{
+					$old_url        = $media->url;
+					$replacement    = '<a href="'.$media->expanded_url.'" rel="external" class="twitter-media" data-media="'.$media->media_url.'">'.$media->display_url.'</a>';
+					$output         = str_replace($old_url, $replacement, $output);
+				}
+			}
+		}
+
+		return $output;
+	}
+
+
+    public function get_tweets( $username, $count=3, $consumer_key, $consumer_secret, $user_token, $user_secret ) {
+
+        if ( ! $tweets = get_transient( 'smf_tweets' ) ) {
+            
+            $tmhOAuth = new tmhOAuth(array(
+                'consumer_key'    => $consumer_key,
+                'consumer_secret' => $consumer_secret,
+                'user_token'      => $user_token,
+                'user_secret'     => $user_secret,
+            ));
+
+			/* Note that we get 20 Tweets by default here, as we don't include replies.  The way
+			 * that this works is Twitter gets 'count' Tweets, and then filters out replies. So if you
+			 * get 5 Tweets and the last 5 Tweets were all replies, then you'll get nothing.
+			 */
+			 $code = $tmhOAuth->request('GET', $tmhOAuth->url('1.1/statuses/user_timeline'), array(
+                'screen_name' => $username,
+                'count' => 20,
+                'exclude_replies' => true ));
+            if ($code == 200) {
+                $tweets = json_decode($tmhOAuth->response['response']);
+				// Now we slice the required number of tweets.
+				$tweets = array_slice( $tweets, 0, $count );
+			} else {
+                $tweets = array();
+            }
+
+            set_transient('oikos_tweets', $tweets, 5 * 60);
+
+        }
+
+        return $tweets;
+    }
+
+    function widget($args, $instance) {
+				
+        extract( $args );
+        $title = apply_filters('widget_title', $instance['title']);
+		$username = isset($instance['username']) ? $instance['username'] : '';
+		$count = isset($instance['count']) ? $instance['count'] : 3;
+		$consumer_key = isset($instance['consumer_key']) ? $instance['consumer_key'] : '';
+		$consumer_secret = isset($instance['consumer_secret']) ? $instance['consumer_secret'] : '';
+		$user_token = isset($instance['user_token']) ? $instance['user_token'] : '';
+		$user_secret = isset($instance['user_secret']) ? $instance['user_secret'] : '';
+
+		$twitter_url = 'http://twitter.com/' . $username;
+
+		echo $before_widget;
+
+	    if ( $title ) {
+		    	// Construct the title
+		    	$title_output = $before_title;
+		    	
+	    		$title_output .= sprintf('<a href="%s">%s</a>', $twitter_url, $title);
+
+		    	$title_output .= $after_title;
+
+		    	echo $title_output;
+        }
+
+        $tweets = self::get_tweets( $username, $count, $consumer_key, $consumer_secret, $user_token, $user_secret );
+		
+		while ($this_tweet = array_shift($tweets)) {
+?>
+			<div class="tweet-container">
+				<a class="tweet-user-avatar" href="https://twitter.com/intent/user?user_id=<?php echo $this_tweet->user->id_str; ?>">
+					<img width="32" height="32" src="<?php echo $this_tweet->user->profile_image_url; ?>" />
+				</a>
+				<p class="tweet-user-names">
+					<a href="https://twitter.com/intent/user?user_id=<?php echo $this_tweet->user->id_str; ?>">
+						<?php echo $this_tweet->user->name; ?>
+					</a>
+					<a href="https://twitter.com/intent/user?user_id=<?php echo $this_tweet->user->id_str; ?>">
+						@<?php echo $this_tweet->user->screen_name; ?>
+					</a>
+				</p>
+				<p class="tweet-time"><a href="http://twitter.com/<?php echo $this_tweet->user->screen_name; ?>/status/<?php echo $this_tweet->id_str; ?>"><?php echo self::relativeTime( strtotime( $this_tweet->created_at)); ?></a></p>
+				<div class="tweet-text">
+				<?php
+					echo self::linkify_tweet( $this_tweet->text, $this_tweet );
+				?>
+				</div>
+				<div class="tweet-intents">
+					<a class="intent-reply" title="Reply to this Tweet" href="https://twitter.com/intent/tweet?in_reply_to=<?php echo $this_tweet->id_str; ?>">Reply</a>
+					<a class="intent-retweet" title="Retweet this Tweet" href="https://twitter.com/intent/retweet?tweet_id=<?php echo $this_tweet->id_str; ?>">Re-tweet</a>
+					<a class="intent-favorite" title="Favourite this Tweet" href="https://twitter.com/intent/favorite?tweet_id=<?php echo $this_tweet->id_str; ?>">Favourite</a>
+				</div>
+			</div>
+
+	<?php	
+		}
+	?>
+		<a href="https://twitter.com/<?php echo $username; ?>" class="twitter-follow-button" data-show-count="false" data-dnt="true">Follow @<?php echo $username; ?></a>
+	<?php
+		echo $after_widget;
+    }
+
+    /** @see WP_Widget::update */
+    function update($new_instance, $old_instance) {				
+        return $new_instance;
+    }
+
+    /** @see WP_Widget::form */
+    function form($instance) {
+						
+        $title = esc_attr(isset($instance['title']) ? $instance['title'] : "Twitter");
+		$username = isset($instance['username']) ? $instance['username'] : '';
+		$count = isset($instance['count']) ? $instance['count'] : 3;
+		$consumer_key = isset($instance['consumer_key']) ? $instance['consumer_key'] : '';
+		$consumer_secret = isset($instance['consumer_secret']) ? $instance['consumer_secret'] : '';
+		$user_token = isset($instance['user_token']) ? $instance['user_token'] : '';
+		$user_secret = isset($instance['user_secret']) ? $instance['user_secret'] : '';
+		?>
+            <p>This widget displays the latest Tweet</p>
+			<p>
+                <label for="<?php echo $this->get_field_id('title'); ?>">Title:</label>
+                <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
+            </p>
+			<p>
+                <label for="<?php echo $this->get_field_id('username'); ?>">Username:</label>
+                <input class="widefat" id="<?php echo $this->get_field_id('username'); ?>" name="<?php echo $this->get_field_name('username'); ?>" type="text" value="<?php echo $username; ?>" />
+            </p>
+			<p>
+                <label for="<?php echo $this->get_field_id('count'); ?>">Number of tweets:</label>
+                <select id="<?php echo $this->get_field_id('count'); ?>" name="<?php echo $this->get_field_name('count'); ?>" />
+					<?php
+						for ($i=1; $i<=20; $i++) {
+							printf('<option value="%d" %s>%d</option>', $i, selected($i, $count, false), $i);
+						} 
+					?>
+				</select>
+            </p>
+			<p>
+                <label for="<?php echo $this->get_field_id('consumer_key'); ?>">Consumer Key:</label>
+                <input class="widefat" id="<?php echo $this->get_field_id('consumer_key'); ?>" name="<?php echo $this->get_field_name('consumer_key'); ?>" type="text" value="<?php echo $consumer_key; ?>" />
+            </p>
+			<p>
+                <label for="<?php echo $this->get_field_id('consumer_secret'); ?>">Consumer Secret:</label>
+                <input class="widefat" id="<?php echo $this->get_field_id('consumer_secret'); ?>" name="<?php echo $this->get_field_name('consumer_secret'); ?>" type="text" value="<?php echo $consumer_secret; ?>" />
+            </p>
+			<p>
+                <label for="<?php echo $this->get_field_id('user_token'); ?>">User Token:</label>
+                <input class="widefat" id="<?php echo $this->get_field_id('user_token'); ?>" name="<?php echo $this->get_field_name('user_token'); ?>" type="text" value="<?php echo $user_token; ?>" />
+            </p>
+			<p>
+                <label for="<?php echo $this->get_field_id('user_secret'); ?>">User Secret:</label>
+                <input class="widefat" id="<?php echo $this->get_field_id('user_secret'); ?>" name="<?php echo $this->get_field_name('user_secret'); ?>" type="text" value="<?php echo $user_secret; ?>" />
+            </p>
+        
+				<?php 
+    }
+}
+
+add_action('widgets_init', create_function('', 'return register_widget("Oikos_TwitterWidget");'));
---
+++ oikos-tweets.php
@@ -0,0 +1,137 @@
+<?php
+/*
+Plugin Name: Oikos Twitter Widget
+Plugin URI: http://oikos.org.uk
+Description: This is a simple plugin used to get tweets using the Twitter REST API v1.1. and oAuth
+Version: 0.1
+Author: Ross Wintle
+Author Email: ross@oikos.org.uk
+License:
+
+  Copyright 2013 Ross Wintle (ross@oikos.org.uk)
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License, version 2, as 
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+  
+*/
+
+class OikosTwitterWidget {
+
+	/*--------------------------------------------*
+	 * Constants
+	 *--------------------------------------------*/
+	const name = 'Oikos Twitter Widget';
+	const slug = 'oikos_twitter_widget';
+	
+	/**
+	 * Constructor
+	 */
+	function __construct() {
+		//Hook up to the init action
+		add_action( 'plugins_loaded', array( &$this, 'init_oikos_twitter_widget' ) );
+	}
+  
+	/**
+	 * Runs when the plugin is activated
+	 */  
+	function install_oikos_twitter_widget() {
+		// do not generate any output here
+	}
+  
+	/**
+	 * Runs when the plugin is initialized
+	 */
+	function init_oikos_twitter_widget() {
+	
+		// Include the widget class
+		require('class-oikos-twitter-widget.php');
+		
+		// Load JavaScript and stylesheets
+		$this->register_scripts_and_styles();
+
+	
+		if ( is_admin() ) {
+			//this will run when in the WordPress admin
+		} else {
+			//this will run when on the frontend
+		}
+
+		/*
+		 * TODO: Define custom functionality for your plugin here
+		 *
+		 * For more information: 
+		 * http://codex.wordpress.org/Plugin_API#Hooks.2C_Actions_and_Filters
+		 */
+		//add_action( 'your_action_here', array( &$this, 'action_callback_method_name' ) );
+		//add_filter( 'your_filter_here', array( &$this, 'filter_callback_method_name' ) );    
+	}
+
+	/*function action_callback_method_name() {
+		// TODO define your action method here
+	}
+
+	function filter_callback_method_name() {
+		// TODO define your filter method here
+	}
+	*/
+	
+	/**
+	 * Callback to print Twitter API code in footer
+	 */
+	public static function print_twitter_api() {
+?>
+		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+<?
+	}
+
+	/**
+	 * Registers and enqueues stylesheets for the administration panel and the
+	 * public facing site.
+	 */
+	private function register_scripts_and_styles() {
+		if ( is_admin() ) {
+
+		} else {
+			$this->load_file('oikos-twitter-widget-css', 'oikos-twitter-widget.css', false);
+			add_action('wp_footer', array('OikosTwitterWidget', 'print_twitter_api'));
+		} // end if/else
+	} // end register_scripts_and_styles
+	
+	/**
+	 * Helper function for registering and enqueueing scripts and styles.
+	 *
+	 * @name	The 	ID to register with WordPress
+	 * @file_path		The path to the actual file
+	 * @is_script		Optional argument for if the incoming file_path is a JavaScript source file.
+	 */
+	private function load_file( $name, $file_path, $is_script = false ) {
+
+		$url = plugins_url($file_path, __FILE__);
+		$file = plugin_dir_path(__FILE__) . $file_path;
+
+		if( file_exists( $file ) ) {
+			if( $is_script ) {
+				wp_register_script( $name, $url, array('jquery') ); //depends on jquery
+				wp_enqueue_script( $name );
+			} else {
+				wp_register_style( $name, $url );
+				wp_enqueue_style( $name );
+			} // end if
+		} // end if
+
+	} // end load_file
+  
+} // end class
+new OikosTwitterWidget();
+
+?>
